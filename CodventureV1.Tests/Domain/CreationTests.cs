717d6c1ca678cf293a7cd081865d17ce244b1134
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-11-20 09:05:38 +0700

    feat: Add Test project

---
+++ CodventureV1.Tests/Domain/CreationTests.cs
@@ -0,0 +1,95 @@
+﻿using CodventureV1.Domain.Players;
+using CodventureV1.Domain.Skills;
+
+namespace CodventureV1.Tests.Domain;
+
+[TestFixture]
+public class CreationTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void PlayerConstrutor_NoParametersPassed_ReturnsDefaultValue()
+    {
+        // Arrange
+        var player = new Player();
+
+        // Act
+        var actualUserName = player.UserName;
+        var actualEmail = player.Email;
+        var actualTypingSpeed = player.TypingSpeed;
+        var actualLearningSpeed = player.LearningSpeed;
+        var actualPlayerSkills = player.PlayerSkills;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actualUserName, Is.EqualTo(null));
+            Assert.That(actualEmail, Is.EqualTo(null));
+            Assert.That(actualTypingSpeed, Is.EqualTo(PlayerConstants.DefaultTypingSpeed));
+            Assert.That(actualLearningSpeed, Is.EqualTo(PlayerConstants.DefaultLearningSpeed));
+            Assert.That(actualPlayerSkills, Is.EqualTo(new HashSet<PlayerSkill>()));
+        });
+    }
+
+    [Test]
+    public void PlayerConstrutor_NoTypingAndLearningSpeedPassed_ReturnsDefaultValue()
+    {
+        // Arrange
+        var player = new Player("thai", "thai@gmail.com");
+
+        // Act
+        var actualTypingSpeed = player.TypingSpeed;
+        var actualLearningSpeed = player.LearningSpeed;
+        var actualPlayerSkills = player.PlayerSkills;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actualTypingSpeed, Is.EqualTo(PlayerConstants.DefaultTypingSpeed));
+            Assert.That(actualLearningSpeed, Is.EqualTo(PlayerConstants.DefaultLearningSpeed));
+            Assert.That(actualPlayerSkills, Is.EqualTo(new HashSet<PlayerSkill>()));
+        });
+    }
+
+    [Test]
+    public void SkillTypeConstrutor_NoParametersPassed_ReturnsDefaultValue()
+    {
+        // Arrange
+        var skillType = new SkillType();
+
+        // Act
+        var actualName = skillType.Name;
+        var actualCode = skillType.Code;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actualName, Is.EqualTo(string.Empty));
+            Assert.That(actualCode, Is.EqualTo(string.Empty));
+        });
+    }
+
+    [Test]
+    public void SkillConstrutor_NoParametersPassed_ReturnsDefaultValue()
+    {
+        // Arrange
+        var skill = new Skill();
+
+        // Act
+        var actualName = skill.Name;
+        var actualCode = skill.Code;
+        var actualPlayerSkills = skill.PlayerSkills;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actualName, Is.EqualTo(string.Empty));
+            Assert.That(actualCode, Is.EqualTo(string.Empty));
+            Assert.That(actualPlayerSkills, Is.EqualTo(new HashSet<PlayerSkill>()));
+        });
+    }
+}
---
+++ CodventureV1.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CodventureV1.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
